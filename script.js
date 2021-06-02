3167153148e425aeece7ff0acf242f88ef691b1c
HannahJT <>
2021-06-02 15:26:18 +0000

    Adds spaces.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Adds 1 to whatever number you add into the input box
+// Adds 1 to whatever number you add into the input box
 let number = 0
 let sentence = 'hello, this is the number 3.'
 
@@ -7,10 +7,9 @@ document.getElementById('button').addEventListener('click', addition)
 function addition () {
   number = document.getElementById('input').value
 
-number = parseInt(number)
+  number = parseInt(number)
 
   number = number + 1
-
 
   alert(number)
 }
