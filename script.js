4b53a9da666f821c784c11425b4b4850718c8559
HannahJT <>
2021-06-02 15:21:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//Adds 1 to whatever number you add into the input box
+let number = 0
+let sentence = 'hello, this is the number 3.'
+
+document.getElementById('button').addEventListener('click', addition)
+
+function addition () {
+  number = document.getElementById('input').value
+
+number = parseInt(number)
+
+  number = number + 1
+
+
+  alert(number)
+}
